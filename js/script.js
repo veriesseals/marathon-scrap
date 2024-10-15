4654e06853857dd1155391756e7cc53089e14cd2
Veries Seals <veries@me.com>
2024-10-14 21:11:53 -0500

    Changed logo and Readded Nav menu

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// var i = 300;
+
+// if (i < 200) {
+//     console.log ("Yayyyyyy!")
+// }
+//  else {
+//     console.log ("Booooooo!")
+//  }
+
+// console.log(i);
+
+
+// This Defines Hour
+// -------------------------------------------------
+let hour = 17;
+
+
+// This is the Conditional
+// -------------------------------------------------
+if (hour < 18) {
+    greeting = "Good day";
+  } 
+  else {
+    greeting = "Good evening";
+  }
+
+// This is the output:
+// -------------------------------------------------
+  console.log(greeting);
